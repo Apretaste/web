cfc860a2cfdc18fb4443849600d67cd6dc3a1510
Salvi Pascual <salvi.pascual@gmail.com>
2019-03-30 01:39:48 -0400

    almost ready

--- web.sql
+++ web.sql
@@ -1,20 +1,26 @@
-CREATE TABLE _navegar_searchs(
-	search_source varchar(10),
-	search_query varchar(255),
-	last_usage timestamp DEFAULT CURRENT_TIMESTAMP,
-	usage_count int(11) DEFAULT 1,
-	PRIMARY KEY (search_source, search_query)
-);
+--
+-- Table structure for table `_web_cache`
+--
 
-CREATE TABLE _navegar_visits(
-	site varchar(255) PRIMARY KEY,
-	last_usage timestamp DEFAULT CURRENT_TIMESTAMP,
-	usage_count int(11) DEFAULT 1
-);
+CREATE TABLE `_web_cache` (
+  `id` int(11) NOT NULL,
+  `url_hash` char(32) NOT NULL,
+  `url` varchar(250) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `visits` int(11) NOT NULL DEFAULT '1',
+  `inserted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE _web_sites(
-	domain varchar(50) not null primary key,
-	title varchar(255),
-	inserted timestamp default current_timestamp,
-	owner varchar(255)
-);
+--
+-- Indexes for table `_web_cache`
+--
+ALTER TABLE `_web_cache`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `url_hash` (`url_hash`);
+
+--
+-- AUTO_INCREMENT for table `_web_cache`
+--
+ALTER TABLE `_web_cache`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
