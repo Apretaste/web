1851f1f1e3926077cccb73665f087499c64378b1
kumahacker <kumahavana@gmail.com>
2019-05-01 15:56:47 -0500

    entire web page

--- scripts.js
+++ scripts.js
@@ -20,6 +20,9 @@ $(document).ready(function(){
 			$('#saveModeMessage').css('display', 'none');
 		}
 	}
+
+	if (typeof html != 'undefined')
+		$("#container-frame").setAttribute('srcdoc', html);
 });
 
 //
