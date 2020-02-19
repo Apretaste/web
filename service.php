0c99aca32cec6d40956bb6c8cff8681e1a2a23bc
kumahacker <kumahavana@gmail.com>
2020-02-19 18:30:52 -0500

    fix image

--- service.php
+++ service.php
@@ -80,7 +80,7 @@ class Service
 				'mobile_user_agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16',
 				'max_attachment_size' => 400,
 				'cache_life_time' => 100000,
-			]);
+			],((int)$settings->save_mode) === 1);
 
 			$html = '';
 			if ($info !== false) {
