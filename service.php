0e2c70dc11f91141de3046c29550c5898edb68e9
kumahacker <kumahavana@gmail.com>
2020-05-24 11:55:49 -0300

    process page

--- service.php
+++ service.php
@@ -396,7 +396,7 @@ class Service
 
 		// get page from DOM
 		$page = $doc->saveHTML();
-		$page = strip_tags($page, '<html><meta><body><head><script><style><a><p><label><div><pre><h1><h2><h3><h4><h5><button><i><b><u><li><ol><ul><fieldset><small><legend><form><input><span><button><nav><table><tr><th><td><thead><img><link>');
+		//$page = strip_tags($page, '<html><meta><body><head><script><style><a><p><label><div><pre><h1><h2><h3><h4><h5><button><i><b><u><li><ol><ul><fieldset><small><legend><form><input><span><button><nav><table><tr><th><td><thead><img><link>');
 
 		return [
 		  'page' => $page,
