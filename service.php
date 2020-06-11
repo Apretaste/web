3b0e3f001758f816ffe5bebfda4999d52ca3f6e7
kumahacker <kumahavana@gmail.com>
2020-06-11 17:36:16 -0300

    update

--- service.php
+++ service.php
@@ -429,7 +429,7 @@ class Service
 
 		// get page from DOM
 		$page = $doc->saveHTML();
-		$page = str_replace(urlencode($appResources), $appResources, $page);
+		$page = utf8_encode(str_replace(urlencode($appResources), $appResources, $page));
 
 		//$page = strip_tags($page, '<html><meta><body><head><script><style><a><p><label><div><pre><h1><h2><h3><h4><h5><button><i><b><u><li><ol><ul><fieldset><small><legend><form><input><span><button><nav><table><tr><th><td><thead><img><link>');
 
